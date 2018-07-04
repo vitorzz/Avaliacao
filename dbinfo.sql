909f5ff84b552b0eb1704b4c853da9f2cc67638a
vitorzz <38111280+vitorzz@users.noreply.github.com>
2018-07-04 16:20:49 -0300

    Add files via upload

---
+++ dbinfo.sql
@@ -0,0 +1,19 @@
+use dbinfox;
+show tables;
+create table tb_usuario(
+iduser int primary key,
+usuario varchar(50),
+fone varchar(50) not null,
+login varchar(50) not null unique,
+senha varchar(50) not null,
+perfil varchar(50) not null);
+show tables;
+describe tb_usuario;
+insert into tb_usuario values
+(1,'Vitor Souza Zelli','1111-1111','vitorz','123456','aluno');
+insert into tb_usuario values
+(2,'Luquinhas','2222-2222','Luqueba','123456','aluno');
+insert into tb_usuario values
+(3,'Benisto Jeans','3333-3333','Benisto','123456','Heroi');
+select * from tb_usuario;
+
